69eab7d408a6ccf220cc97dea71ac108676db90e
Dylan YEUNG <79907161+Yushirenko@users.noreply.github.com>
2021-09-14 19:42:46 +0800

    Add files via upload
    V 0.0.1

---
+++ include/insrobo/qnode.hpp
@@ -0,0 +1,79 @@
+/**
+ * @file /include/insrobo/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef insrobo_QNODE_HPP_
+#define insrobo_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+// To workaround boost/qt4 problems that won't be bugfixed. Refer to
+//    https://bugreports.qt.io/browse/QTBUG-22829
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#endif
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace insrobo {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+Q_SIGNALS:
+	void loggingUpdated();
+    void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+	ros::Publisher chatter_publisher;
+    ros::Subscriber chatter_subscriber;
+    QStringListModel logging_model;
+
+    void chatter_callback(const std_msgs::String &msgs);
+};
+
+}  // namespace insrobo
+
+#endif /* insrobo_QNODE_HPP_ */
